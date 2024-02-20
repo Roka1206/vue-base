64638c8a5829bb05bfbfece3254d9db3532a2f66
Roka1206 <102240998+Roka1206@users.noreply.github.com>
2024-02-20 12:59:28 +0100

    creando estructura y input base

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  modeValue: {
+    type: String,
+    default: ''
+  }
+})
+
+const emit = defineEmits(['update:modeValue'])
+
+const localValue = computed({
+  get() {
+    return props.modelValue
+  },
+  set(newValue) {
+    emit('update:modeValue', newValue)
+  }
+})
+</script>
+
+<template>
+  <input v-model="localValue" />
+</template>
+
+<style scoped></style>
